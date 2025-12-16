127055ff03860221621928098a2a2d1693f7a1f6
guy-roh <lizgony@gmail.com>
2025-12-16 23:31:47 +0100

    feat: enviroment hdri

--- src/components/UI/MainApp.tsx
+++ src/components/UI/MainApp.tsx
@@ -2,23 +2,36 @@
 
 import { Canvas } from '@react-three/fiber'
 import { useState } from 'react'
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+
+
+const FallBack = () => {
+    return <div>Loading...</div>
+}
 
 
 const MainApp = () => {
-  return (
-    <Canvas
-    className='main-app'
-    >
-        MainApp
+    return (
+        <Canvas
+            className='main-app'
+            fallback={<FallBack />}
+            orthographic
+            camera={{ 
+                zoom: 100, 
+                position: [10, 10, 10] 
+            }}
+        >
+            <Environment
+                files="/img/hdri/sky.hdr"
+                background
+            />
             <OrbitControls />
-            <ambientLight intensity={0.5} />
-        <mesh>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-    </Canvas>
-  )
+            <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+            </mesh>
+        </Canvas>
+    )
 }
 
 export default MainApp
