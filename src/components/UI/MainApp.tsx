ffb36eb9825cc78c7ca16f5b55df41ae2b980358
guy-roh <lizgony@gmail.com>
2025-12-19 23:38:53 +0100

    feat: test model

--- src/components/UI/MainApp.tsx
+++ src/components/UI/MainApp.tsx
@@ -4,10 +4,36 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from 'three';
 import testData from "@/data/testData.json";
+import {
+    MeshTransmissionMaterial, ContactShadows, useGLTF, Environment
+} from '@react-three/drei';
 
 const FallBack = () => <div>Loading...</div>
 
 const MainApp = () => {
+
+    const SphereMesh = ({
+        position
+    }: { position: [number, number, number] }) => {
+        const { nodes }: any = useGLTF('/models/sphere.glb');
+        return (
+            <mesh
+                geometry={nodes.Sphere.geometry}
+                position={[0, 0, 0]}
+                castShadow
+                receiveShadow
+            >
+                <MeshTransmissionMaterial
+                    transmission={1}
+                    thickness={2}
+                    roughness={0}
+                    chromaticAberration={0.1}
+                    anisotropy={0.1}
+                />
+            </mesh>
+        )
+    }
+
     return (
         <Canvas
             className='main-app'
@@ -19,12 +45,14 @@ const MainApp = () => {
                 position: [10, 10, 10]
             }}
         >
+            <ContactShadows scale={100} position={[0, -7.5, 0]} blur={1} far={100} opacity={0.85} />
+
             <OrbitControls enableRotate={false} />
             <ambientLight intensity={0.5} />
             <directionalLight
-                position={[10, 10, 5]}
-                intensity={1} 
                 castShadow
+                position={[10, 10, 5]}
+                intensity={1}
                 shadow-camera-left={-10}
                 shadow-camera-right={10}
                 shadow-camera-top={10}
@@ -39,7 +67,13 @@ const MainApp = () => {
                     receiveShadow
                 >
                     <boxGeometry args={[1, 1, 1]} />
-                    <meshStandardMaterial color="white" />
+                    <MeshTransmissionMaterial
+                        transmission={1}
+                        thickness={2}
+                        roughness={0}
+                        chromaticAberration={0.1}
+                        anisotropy={0.1}
+                    />
                 </mesh>
             ))}
             <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
