3affbf59d4ed1701fa04d2d4aae95219df93f9af
guy-roh <lizgony@gmail.com>
2025-12-19 01:13:48 +0100

    test: PFCSoftShadow

--- src/components/UI/MainApp.tsx
+++ src/components/UI/MainApp.tsx
@@ -2,57 +2,51 @@
 
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from "@react-three/drei";
+import * as THREE from 'three';
 import testData from "@/data/testData.json";
 
-const FallBack = () => {
-    return <div>Loading...</div>
-}
-const MainApp = () => {
+const FallBack = () => <div>Loading...</div>
 
+const MainApp = () => {
     return (
-        <>
-            <Canvas
-                className='main-app'
-                fallback={<FallBack />}
-                orthographic
-                shadows
-                camera={{
-                    zoom: 100,
-                    position: [10, 10, 10]
-                }}
-            >
-                <OrbitControls
-                    enableRotate={false}
-                />
-                <ambientLight intensity={0.5} />
-                <directionalLight
-                    position={[10, 10, 5]}
-                    intensity={10}
-                    shadow-camera-near={0.1}
-                    shadow-camera-far={25}
+        <Canvas
+            className='main-app'
+            fallback={<FallBack />}
+            orthographic
+            shadows={{ type: THREE.PCFSoftShadowMap }}
+            camera={{
+                zoom: 100,
+                position: [10, 10, 10]
+            }}
+        >
+            <OrbitControls enableRotate={false} />
+            <ambientLight intensity={0.5} />
+            <directionalLight
+                position={[10, 10, 5]}
+                intensity={1} 
+                castShadow
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+                shadow-mapSize={[1024, 1024]}
+            />
+            {testData.testStations.map((station: any) => (
+                <mesh
+                    key={station.id}
+                    position={station.stationPosition}
                     castShadow
-                    shadow-mapSize-width={1024}
-                    shadow-mapSize-height={1024}
-                />
-                {
-                    testData.testStations.map((station: any) => (
-                        <mesh
-                            position={station.stationPosition}
-                            castShadow
-                            receiveShadow
-                            key={station.id}
-                        >
-                            <boxGeometry args={[1, 1, 1]} />
-                            <meshStandardMaterial color="white" />
-                        </mesh>
-                    ))
-                }
-                <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
-                    <planeGeometry args={[100, 100]} />
-                    <meshStandardMaterial color="lightgrey" />
+                    receiveShadow
+                >
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial color="white" />
                 </mesh>
-            </Canvas>
-        </>
+            ))}
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+                <planeGeometry args={[100, 100]} />
+                <meshStandardMaterial color="lightgrey" />
+            </mesh>
+        </Canvas>
     )
 }
 
