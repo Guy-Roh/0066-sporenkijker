9e27570be496a943246c56766bcfa6af9cefdedb
guy-roh <lizgony@gmail.com>
2026-01-13 17:05:35 +0100

    type: nodes

--- src/components/Helpers/Trains.ts
+++ src/components/Helpers/Trains.ts
@@ -1,6 +1,7 @@
 import { Train, TrainData } from "@/app/type";
+import { Object3D } from "three";
 
-export const getTrainPosition = (platformNumber: string, stationId: string, meshNodes: any) => {
+export const getTrainPosition = (platformNumber: string, stationId: string, meshNodes: Record<string, Object3D>) => {
     const cleanStationId = stationId.replace(/\./g, "");
     const paddedPlatform = platformNumber.toString().padStart(3, "0");
     const trainKey = `${cleanStationId}${paddedPlatform}`;
@@ -17,7 +18,7 @@ export const getTrainPosition = (platformNumber: string, stationId: string, mesh
     }
 }
 
-export const filterTrains = (meshNodes: any, trainsData: TrainData) => {
+export const filterTrains = (meshNodes: Record<string, Object3D>, trainsData: TrainData) => {
     const rawStationId = trainsData?.station || "";
     const trains = trainsData?.trains || [];
 
@@ -25,8 +26,8 @@ export const filterTrains = (meshNodes: any, trainsData: TrainData) => {
 
     const validTrainKeys = new Set(
         trains
-            .filter((t: any) => t.platform && t.platform !== "?")
-            .map((t: any) => {
+            .filter((t: Train) => t.platform && t.platform !== "?")
+            .map((t: Train) => {
                 const paddedPlatform = t.platform
                     .toString()
                     .padStart(3, "0");
@@ -34,7 +35,8 @@ export const filterTrains = (meshNodes: any, trainsData: TrainData) => {
             })
     ); // this will hold keys like "BENMBS008821006001"
 
-    const selectedNodes: any = {};
+    const selectedNodes: Record<string, Object3D> = {};
+    console.log("The selected nodes are", selectedNodes);
     
     for (const [name, node] of Object.entries(meshNodes)) {
         if (name.startsWith("BENMBS")) {
