beabf249a70ff1d3893f6ee7380775d1c548c2e2
guy-roh <lizgony@gmail.com>
2026-01-18 23:50:36 +0100

    feat: webgpu attempt

--- src/components/Helpers/GetData.ts
+++ src/components/Helpers/GetData.ts
@@ -5,10 +5,6 @@ export const fetchTrainData = async (stationId: string): Promise<TrainData> => {
     //const api = "dummy"; // Use dummy API for testing
     const api = "trains"
 
-    const response = await fetch(`api/${api}?stationId=${encodeURIComponent(stationId)}`, {
-        cache: 'no-store'
-    });
-
     if (!response.ok) {
         throw new Error(`Failed to fetch: ${response.status}`);
     }
